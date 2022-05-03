0788c04ceba842ccd1cbc285eecaaf4e3848cb63
Toni <tony.kunstek@gmail.com>
2022-05-03 22:37:38 +0200

    Space Breaker patch 3

--- Space Breaker/Assets/Scripts/Ball.cs
+++ Space Breaker/Assets/Scripts/Ball.cs
@@ -2,20 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class Ball : MonoBehaviour
 {
     public Paddle paddle;
     public AudioClip[] ballSounds;
+
     private Vector2 paddleToBallVector;
     private AudioSource myAudioSource;
 
     private bool hasStarted = false;
+
+    float constantSpeed = 12f;
+
 
     // Start is called before the first frame update
     void Start()
     {
         paddleToBallVector = transform.position - paddle.transform.position;
         myAudioSource = GetComponent<AudioSource>();
+        Debug.Log(ballSounds[0].name);
     }
 
     // Update is called once per frame
@@ -24,19 +31,22 @@ public class Ball : MonoBehaviour
         if(!hasStarted){            
             Vector2 paddlePos = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
             transform.position = paddlePos + paddleToBallVector;
-        }
+        }  
 
-        if(Input.GetMouseButtonDown(0)){
+        if(!hasStarted && Input.GetMouseButtonDown(0)){
             hasStarted = true;
             GetComponent<Rigidbody2D>().velocity = new Vector2(2f, 10f);
+            GetComponent<Rigidbody2D>().velocity = constantSpeed * (GetComponent<Rigidbody2D>().velocity.normalized);
         }
     }
 
     void OnCollisionEnter2D(Collision2D collision2D){
-        Vector2 tweakVelocity = new Vector2(Random.Range(0f,0.2f), Random.Range(0f, 0.2f));
+        Vector2 tweakVelocity = new Vector2(Random.Range(-0.15f,0.15f), Random.Range(-0.15f,0.15f));
+        Debug.DrawLine(this.transform.position,new Vector3(tweakVelocity.normalized.x, tweakVelocity.normalized.y,0)*100f, Color.red, 10f);
+        Vector2 targetDirection = (GetComponent<Rigidbody2D>().velocity+tweakVelocity).normalized;
         if(hasStarted){
-            GetComponent<Rigidbody2D>().velocity += tweakVelocity;
-            AudioClip clip = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
+            GetComponent<Rigidbody2D>().velocity = targetDirection * constantSpeed;
+            AudioClip clip = ballSounds[0];
             myAudioSource.PlayOneShot(clip);
         }
     }
